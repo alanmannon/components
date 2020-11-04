8a40feaea3be5fc3c66142fcaf82f061723e6504
Alan <alanmannon@gmail.com>
2020-11-03 19:07:56 -0600

    finished exercise on passing props and components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,22 @@
 import React from "react";
-import faker from "faker";
+// import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
+          <img alt="avatar" src={props.avatarURL} />
         </a>
         <div className="content">
-          <a href="/" className="author">Sam</a>
+          <a href="/" className="author">
+            {props.author}
+          </a>
           <div className="metaData">
-            <span className="date">Today at 6:00PM</span>
+            <span className="date">{props.timeDate}</span>
           </div>
-          <div className="text">Nice blog post!
-            </div>
+          <div className="text">{props.comment}
+          </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <div>
+          <h1>Warning?</h1>
+        Are You SURE you want this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeDate="Today at 4:45PM"
+          comment="I didn't say anything"
+          avatarURL={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Frank"
+          timeDate="Today at 4:35PM"
+          comment="I said something too"
+          avatarURL={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jam"
+          timeDate="Yesterday at 4:34PM"
+          comment="I said something"
+          avatarURL={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
